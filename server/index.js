963c35ff4e389f2880e980fae69294cfc7984c5d
D5CD1198KDF <akshaydhayalkd99@gmail.com>
2024-04-05 12:57:43 +0530

    Initial test server code

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.status(201).json({msg:"Hello World"});
+})
+
+app.listen(3000,()=>{
+    console.log("server is running at port 3000");
+})
