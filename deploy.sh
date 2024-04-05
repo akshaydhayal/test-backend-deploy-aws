7c1649c93542bf0f27eb69b953df2ee30673bfac
D5CD1198KDF <akshaydhayalkd99@gmail.com>
2024-04-06 05:07:59 +0530

    modiefied deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
-cd testDeployServer/server
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v21.7.2/bin
+cd test-backend-deploy-aws
 git pull origin main
+cd server
 npx pm2 kill
 npm install
 npx pm2 start index.js
